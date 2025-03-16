8a7563fb4c937fe360d76b61031eac715211fd34
Krushnakanta Nayak <krushnakanta678@gmail.com>
2025-03-16 10:24:12 +0530

    Add image parameter and updated functionality

--- lib/src/buttons/miny_button.dart
+++ lib/src/buttons/miny_button.dart
@@ -1,40 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:miny_design_system/miny_design_system.dart';
 
 /// A custom button for Miny Design System.
 class MinyButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
   final bool isLoading;
+  final ImageProvider? icon;
 
   const MinyButton({
     super.key,
     required this.label,
     required this.onPressed,
     this.isLoading = false,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return ElevatedButton(
       onPressed: isLoading ? null : onPressed,
       style: ButtonStyle().copyWith(
         backgroundColor: WidgetStateProperty.all(
-          isLoading ? Color(0xff757376) : Color(0xff1F1E24),
+          isLoading ? Color(0xff757376) : theme.colors.neutralBackground,
         ),
       ),
       child: isLoading
           ? SizedBox(
-              height: 25,
-              width: 25,
+              height: theme.sizing.height.s8,
+              width: theme.sizing.height.s8,
               child: CircularProgressIndicator(
                 strokeWidth: 2.5,
                 strokeCap: StrokeCap.round,
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                valueColor:
+                    AlwaysStoppedAnimation<Color>(theme.colors.neutralLight),
               ),
             )
-          : Text(
-              label,
-              style: TextStyle(color: Colors.white),
+          : Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image(
+                  height: theme.sizing.height.s7,
+                  width: theme.sizing.height.s7,
+                  image: icon!,
+                ),
+                SizedBox(width: theme.spacing.width.s12),
+                Text(
+                  label,
+                  style: theme.textStyle.bodyMedium.copyWith(
+                    color: theme.colors.neutralLight,
+                  ),
+                ),
+              ],
             ),
     );
   }
