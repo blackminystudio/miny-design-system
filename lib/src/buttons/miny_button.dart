46118c6e8aa5b2155e4653659900f1e0b534fa3c
Krushnakanta Nayak <krushnakanta678@gmail.com>
2025-05-10 11:29:48 +0530

    remove unnecessary code part

--- lib/src/buttons/miny_button.dart
+++ lib/src/buttons/miny_button.dart
@@ -66,17 +66,3 @@ class MinyButton extends StatelessWidget {
     );
   }
 }
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
